e5730f7a45839d86dca42c55f3e2462c90cbffff
Krishnendu Dasgupta <72221973+KrishnenduDG@users.noreply.github.com>
2023-12-09 02:02:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
-import { spawn } from "node:child_process";
-
+import { exec } from "node:child_process";
 const randGen = (high, low) => Math.floor(Math.random() * (high - low) + low);
 
 const app = express();
@@ -8,15 +7,54 @@ const PORT = 8080;
 
 app.get("/", (req, res) => res.json({ a: 1, b: 2 }));
 
-app.get("/fire", async (req, res) => {
-  try {
-    const ls = spawn(`ls`);
-    ls.stdout.on("data", (data) => {
-      res.json({ status: false, msg: "Success", data: String(data) });
+app.get("/power-on", async (req, res) => {
+  // Run 'ls' command
+  exec("cd ..", (error, lsOutput, lsError) => {
+    if (error) {
+      console.error(`Error executing 'ls': ${error}`);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    // Run 'echo Hello' command
+    exec("sudo python poweron.py", (error, echoOutput, echoError) => {
+      if (error) {
+        console.error(`Error executing 'echo Hello': ${error}`);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      // Send the combined output as the response
+      res.send(
+        `Listing files:\n${lsOutput}\nOutput of 'echo Hello': ${echoOutput}`
+      );
     });
-  } catch (error) {
-    res.json({ status: false, msg: "Failure" });
-  }
+  });
+});
+
+app.get("/power-on", async (req, res) => {
+  // Run 'ls' command
+  exec("cd ..", (error, lsOutput, lsError) => {
+    if (error) {
+      console.error(`Error executing 'ls': ${error}`);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    // Run 'echo Hello' command
+    exec("sudo python powerff.py", (error, echoOutput, echoError) => {
+      if (error) {
+        console.error(`Error executing 'echo Hello': ${error}`);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      // Send the combined output as the response
+      res.send(
+        `Listing files:\n${lsOutput}\nOutput of 'echo Hello': ${echoOutput}`
+      );
+    });
+  });
 });
 
 app.listen(PORT, () => {
